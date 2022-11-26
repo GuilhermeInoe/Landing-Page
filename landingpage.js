fdbf4ff6d74db31dd4548fb48e2b38adf858a395
Guilherme Inoe <guilhermeinoe82@gmail.com>
2022-11-26 16:48:11 -0300

    Landing page

--- landingpage.js
+++ landingpage.js
@@ -2,9 +2,9 @@ function mostrarmenu(){
     let menumobile = document.querySelector('.menu-mobile');
     if(menumobile.classList.contains('open')){
         menumobile.classList.remove('open');
-        document.querySelector('.icone').src = "menu_white_36dp.svg";
+        document.querySelector('.icone').src = "fotos/menu_white_36dp.svg";
     }else{
         menumobile.classList.add('open');
-        document.querySelector('.icone').src = "close_white_36dp.svg";
+        document.querySelector('.icone').src = "fotos/close_white_36dp.svg";
     }
 }
